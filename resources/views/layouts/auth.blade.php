1274df683389207e87f3e38a494624ba179adb3e
zufarf <mzufarfainan@gmail.com>
2024-07-26 04:58:45 +0700

    remove unnecessary lib

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -23,10 +23,7 @@
         <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
         <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-        <link href="/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-        <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
         <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-        <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
         <!-- Template Main CSS File -->
         <link href="/assets/css/style.css" rel="stylesheet">
@@ -45,14 +42,7 @@
                 class="bi bi-arrow-up-short"></i></a>
 
         <!-- Vendor JS Files -->
-        <script src="/assets/vendor/apexcharts/apexcharts.min.js"></script>
         <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/vendor/chart.js/chart.umd.js"></script>
-        <script src="/assets/vendor/echarts/echarts.min.js"></script>
-        <script src="/assets/vendor/quill/quill.js"></script>
-        <script src="/assets/vendor/simple-datatables/simple-datatables.js"></script>
-        <script src="/assets/vendor/tinymce/tinymce.min.js"></script>
-        <script src="/assets/vendor/php-email-form/validate.js"></script>
 
         <!-- Template Main JS File -->
         <script src="/assets/js/main.js"></script>
